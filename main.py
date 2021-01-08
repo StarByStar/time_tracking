b7961f2e2503b2761346b533e9d26d9b3ab65db3
Konstantin <starbystar@inbox.ru>
2021-01-08 21:27:51 +0500

    report added

--- main.py
+++ main.py
@@ -2,30 +2,38 @@ from activity import Activity
 import sqlite3
 from datetime import datetime
 from tkinter import *
+import tkinter.ttk as ttk
 
 db = sqlite3.connect("database.db")
 cursor = db.cursor()
 
-# Счетчик для таймера
-sec = 0
-
 # идентификатор действия для таймера
 after_id = ''
 
+# Счетчик для таймера
+sec = 0
+mm = 58
+hh = 0
+
 # Параметры кнопок
 h = 2
-w = 9
+w = 10
 btnName = "start / stop"
 
 
 # Счетчик таймера
 def tick():
-    global sec, after_id, stopWatchDict
+    global after_id, sec, mm, hh
     after_id = root.after(1000, tick)
-    f_sec = datetime.fromtimestamp(sec).strftime("%M:%S")
+    if sec > 59:
+        if mm >= 59:
+            hh += 1
+            mm = -1
+        mm += 1
+        sec = 0
+    f_sec = str(hh) + ':' + str(mm) + ':' + str(sec)
     Timer['text'] = str(f_sec)
     sec += 1
-
 
 # Завершить таймер
 def stop_timer():
@@ -46,7 +54,9 @@ def startActivity(activity_type):
 # функция для отключения кнопок
 def changeBtnState(buttonName):
     if Activity.running:
-        # Запустить таймер
+        # Обнулить счетчик таймера, запустить таймер
+        global sec, mm, hh
+        sec = mm = hh = 0
         tick()
         # Отключить все кнопки кроме нажатой
         for button in buttonList:
@@ -60,11 +70,52 @@ def changeBtnState(buttonName):
         stop_timer()
 
 
+# Функция сбора отчета , в параметрах передаются часы
+def report(hours):
+    activity_name ="Название активности"
+    avg_duration = "Средняя продолжительность"
+    avg_percent = "Средняя доля"
+    avg_count = "Среднее кол-во"
+    # Нужно прикрутить часы к выборке, в зависимости от типа вызванного отчета
+    cursor.execute("SELECT DISTINCT AT.ACTIVITIES_NAME,"
+                          " ROUND(SUM(CAST (STRFTIME('%s', ACTS.END_DATE) AS FLOAT) - STRFTIME('%s', ACTS.START_DATE) ) / 3600, 2) AS AVGTIME,"
+                          " ROUND( (SUM(CAST (STRFTIME('%s', ACTS.END_DATE) AS FLOAT) - STRFTIME('%s', ACTS.START_DATE) ) / 3600) / 24 * 100, 2) AS AVGPERCENT,"
+                          " COUNT( * ) AS CNT "
+                          "FROM ACTIVITIES ACTS, "
+                          "     ACTIVITIES_TYPES AT "
+                          "WHERE 1 = 1 "
+                          "     AND END_DATE > DATETIME('now', 'localtime', '-24 hours') "
+                          "     AND ACTS.ACTIVITY_TYPE = AT.ID "
+                          "GROUP BY AT.ACTIVITIES_NAME "
+                          "ORDER BY AT.ACTIVITIES_NAME",)
+    data = cursor.fetchall()
+
+    # Начало цикла отрисовки формы с отчетом
+    report = Tk()
+    table = ttk.Treeview(report)
+
+    for rec in data:
+       table.insert('', 'end', values=rec)
+
+    print(data)
+    table["columns"] = [activity_name, avg_duration, avg_percent, avg_count]
+    table["show"] = "headings"
+
+    # Заголовки
+    table.heading(activity_name, text=activity_name)
+    table.heading(avg_duration, text=avg_duration)
+    table.heading(avg_percent, text=avg_percent)
+    table.heading(avg_count, text=avg_count)
+
+    table.grid(row=1, column=1, columnspan=5)
+
+    report.mainloop()
+
 # Начать цикл отрисовки UI
 root = Tk()
 
 # Размер основной формы
-root.geometry("300x250")
+root.geometry("402x250")
 
 # Заголовок
 root.title("time tracking")
@@ -73,7 +124,7 @@ root.title("time tracking")
 root.iconbitmap('mpv-icon.ico')
 
 # Элементы интерфейса
-Timer = Label(root, font=("Courier", 44), text='00:00')
+Timer = Label(root, font=("Courier", 50), text='0:0:0')
 
 sleepLabel = Label(root, text="Sleep")
 sleepButton = Button(root, height=h, width=w, text=btnName, command=lambda: (startActivity(1), changeBtnState(sleepButton)))
@@ -87,8 +138,17 @@ educationButton = Button(root, height=h, width=w, text=btnName, command=lambda: 
 entertainmentLabel = Label(root, text="Entertainment")
 entertainmentButton = Button(root, height=h, width=w, text=btnName, command=lambda: (startActivity(4), changeBtnState(entertainmentButton)))
 
+workLabel = Label(root, text="Work")
+workButton = Button(root, height=h, width=w, text = btnName, command=lambda: (startActivity(5), changeBtnState(workButton)))
+
+reportLabel = Label(root, font=("Courier", 24), text="Reports")
+report1dButton = Button(root, height=h, width=w, text="1 day", command=lambda: (report(24)))
+report7dButton = Button(root, height=h, width=w, text="7 days", command=lambda: (report(168)))
+report30dButton = Button(root, height=h, width=w, text="30 days", command=lambda: (report(720)))
+
+
 # лист кнопок для управления их состоянием
-buttonList = [sleepButton, trainingButton, educationButton, entertainmentButton]
+buttonList = [sleepButton, trainingButton, educationButton, entertainmentButton, workButton]
 
 # Разметка
 Timer.grid(row=1, column=1, columnspan=5)
@@ -99,10 +159,18 @@ sleepButton.grid(row=3, column=1)
 trainingLabel.grid(row=2, column=2)
 trainingButton.grid(row=3, column=2)
 
+workLabel.grid(row=2, column=3)
+workButton.grid(row=3, column=3)
+
 educationLabel.grid(row=2, column=4)
 educationButton.grid(row=3, column=4)
 
 entertainmentLabel.grid(row=2, column=5)
 entertainmentButton.grid(row=3, column=5)
+
+reportLabel.grid(row=4, column=1, columnspan=5)
+report1dButton.grid(row=5, column=1)
+report7dButton.grid(row=5, column=2)
+report30dButton.grid(row=5, column=3)
 
 root.mainloop()
